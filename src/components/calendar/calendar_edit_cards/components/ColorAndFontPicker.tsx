321e868e7c1ada6a4a7112bf39b6a039f47245f3
holpet <45787742+holpet@users.noreply.github.com>
2023-04-27 10:41:44 +0200

    build error fixed

--- src/components/calendar/calendar_edit_cards/components/ColorAndFontPicker.tsx
+++ src/components/calendar/calendar_edit_cards/components/ColorAndFontPicker.tsx
@@ -1,4 +1,4 @@
-import React, { Dispatch, SetStateAction } from "react";
+import { Dispatch, SetStateAction } from "react";
 import { LEGEND_COLORS } from "../../../../lib/modal_utils/modalUtils";
 
 interface IColorAndFontPickerProps {

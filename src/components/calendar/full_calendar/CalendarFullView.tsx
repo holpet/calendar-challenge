317f2645cde2ba42dafb76ad7dac033324229ccd
holpet <45787742+holpet@users.noreply.github.com>
2023-04-29 14:03:24 +0200

    alert delete

--- src/components/calendar/full_calendar/CalendarFullView.tsx
+++ src/components/calendar/full_calendar/CalendarFullView.tsx
@@ -75,7 +75,6 @@ export const CalendarFullView = () => {
     const [event] = events.filter((event) => {
       return event.id === eventDropResizeInfo.event.id;
     });
-    alert(`${eventDropResizeInfo.event.color} ${event.color}`);
     addEditedEventToDB(
       event.title + "",
       dayjs(eventDropResizeInfo.event.start),

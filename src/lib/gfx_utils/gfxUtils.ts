895a5a46aa78a5a335ec242915c03fcb185f8e8d
holpet <45787742+holpet@users.noreply.github.com>
2023-05-01 12:57:59 +0200

    last row tentacle overflow fix

--- src/lib/gfx_utils/gfxUtils.ts
+++ src/lib/gfx_utils/gfxUtils.ts
@@ -6,10 +6,14 @@ export function createTentacleElement(type: string) {
     const today_td = document.querySelector(
       ".fc-day-today .fc-daygrid-day-frame"
     );
-    today_td?.classList.add("relative");
+    const isLastRow =
+      document.querySelector(
+        "table.fc-scrollgrid-sync-table tr[role=row]:last-child .fc-day-today"
+      ) !== null;
     const div = document.createElement("div");
-    div.className =
-      "absolute right-0 top-0 overflow-visible w-4/5 h-auto z-10 pointer-events-none tentacles-today";
+    div.className = `absolute right-0 top-0 w-4/5 ${
+      isLastRow ? "overflow-hidden" : "overflow-visible"
+    } h-full z-10 pointer-events-none tentacles-today`;
     const img = document.createElement("img");
     img.setAttribute("src", tentacles_today);
     img.className = "w-full h-auto";
